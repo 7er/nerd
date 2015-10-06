551c735edc185f7451089abf1506b6319f5afaf6
Syver Enstad <syver.enstad@gmail.com>
2015-10-06 20:50:34 +0200

    Configure development database

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :nerd, Nerd.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "ecto",
+  password: "9797Arne",
   database: "nerd_dev",
   hostname: "localhost",
   pool_size: 10
